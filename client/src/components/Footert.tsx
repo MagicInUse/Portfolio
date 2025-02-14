d4947e3cd69caa86a70e3b1dc36e5a2eb55e54c8
Magic <jacob.norr@live.com>
2025-02-13 23:28:08 -0700

    before merge

--- client/src/components/Footert.tsx
+++ client/src/components/Footert.tsx
@@ -4,10 +4,11 @@ const Footer = () => {
         <ul>
           <li><a href="https://github.com/MagicInUse" target="_blank">GitHub </a></li>
           <li><a href="https://x.com/MagicInUse" target="_blank">X </a></li>
+          <li><a href="https://www.linkedin.com/in/jacob-norr" target="_blank">LinkedIn </a></li>
           <li><a href="https://open.spotify.com/user/magicinuse" target="_blank">Spotify </a></li>
           <li><a href="https://www.twitch.tv/magicinuse" target="_blank">Twitch </a></li>
         </ul>
-        <p>&copy; 2025 | Jacob Norr</p>
+        <p>&copy; {new Date().getFullYear()} | Jacob Norr</p>
       </footer>
     );
   };
