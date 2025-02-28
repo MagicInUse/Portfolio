937651ab0c2d6c857ad86a8dd49127c0d37132fd
MagicInUse <jacob.norr@live.com>
2025-02-28 11:46:12 -0700

    pdfviewer styling and loading props implemented. better resume page altogether

--- client/src/components/PDFViewer/PDFViewer.tsx
+++ client/src/components/PDFViewer/PDFViewer.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-import styles from "./PDFViewer.module.css";
-import { useEffect } from "react";
+import styles from './PDFViewer.module.css';
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
@@ -13,6 +13,7 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ fileUrl }) => {
     const [numPages, setNumPages] = useState<number>();
     const [pageNumber, setPageNumber] = useState<number>(1);
     const [pageWidth, setPageWidth] = useState<number>(Math.min(860, window.innerWidth - 40));
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         function handleResize() {
@@ -25,6 +26,12 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ fileUrl }) => {
 
     function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
         setNumPages(numPages);
+        setError(null);
+    }
+    
+
+    function onDocumentLoadError(error: Error): void {
+        setError(error.message);
     }
 
     function nextPage() {
@@ -49,18 +56,28 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ fileUrl }) => {
                 </button>
             </div>
             <div className={styles.pdfContainer}>
-                <Document
-                    file={fileUrl}
-                    onLoadSuccess={onDocumentLoadSuccess}
-                >
-                    <Page
-                        pageNumber={pageNumber}
-                        renderAnnotationLayer={false}
-                        width={pageWidth}
-                        renderTextLayer={false} // This prop can help reduce extra rendering issues
-                    />
-                </Document>
-            </div>
+    {error ? (
+        <div className={styles.error}>Failed to load PDF: {error}</div>
+    ) : (
+        <>
+            {numPages === undefined && <LoadingSpinner />} {/* Show spinner while loading */}
+            <Document
+                file={fileUrl}
+                onLoadSuccess={onDocumentLoadSuccess}
+                onLoadError={onDocumentLoadError}
+                className={styles.pdfDocument}
+            >
+                <Page
+                    pageNumber={pageNumber}
+                    renderAnnotationLayer={false}
+                    width={pageWidth}
+                    renderTextLayer={false}
+                    className={styles.pdfPage}
+                />
+            </Document>
+        </>
+    )}
+</div>
         </div>
     );
 };
