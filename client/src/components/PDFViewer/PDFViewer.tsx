9c287b983c2af92f76185e467115f90fd5222fdc
MagicInUse <jacob.norr@live.com>
2025-02-27 17:20:28 -0700

    better pdf solution

--- client/src/components/PDFViewer/PDFViewer.tsx
+++ client/src/components/PDFViewer/PDFViewer.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
-import { Viewer, Worker, SpecialZoomLevel, ScrollMode } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import styles from './PDFViewer.module.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 interface PDFViewerProps {
     fileUrl: string;
 }
 
 const PDFViewer: React.FC<PDFViewerProps> = ({ fileUrl }) => {
+    const [numPages, setNumPages] = useState<number>();
+    const [pageNumber, setPageNumber] = useState<number>(1);
+
+    function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+        setNumPages(numPages);
+    }
+
+    function nextPage() {
+        setPageNumber((v) => ++v);
+    }
+
+    function prevPage() {
+        setPageNumber((v) => --v);
+    }
+
     return (
         <div className={styles.viewerContainer}>
-            <Worker workerUrl="//cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js">
-                <Viewer 
-                    fileUrl={fileUrl} 
-                    defaultScale={SpecialZoomLevel.PageWidth}
-                />
-            </Worker>
+            <button onClick={prevPage} disabled={pageNumber <= 1}>
+                Previous
+            </button>
+            <button onClick={nextPage} disabled={pageNumber >= (numPages ?? -1)}>
+                Next
+            </button>
+            <Document
+                file={fileUrl}
+                onLoadSuccess={onDocumentLoadSuccess}
+                className="my-react-pdf"
+            >
+                <Page pageNumber={pageNumber} />
+            </Document>
+            <p>
+                Page {pageNumber} of {numPages}
+            </p>
         </div>
     );
 };
